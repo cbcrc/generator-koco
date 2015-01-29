449680758b160fb8eb1f9dfd0a691a58d2487b0a
W3Max <info@maximeseguin.com>
2015-01-29 15:57:59 -0500

    -

--- app/templates/as-is/src/app/startup.js
+++ app/templates/as-is/src/app/startup.js
@@ -1,11 +1,21 @@
 define([
-        'framework',
-        './components'
+        'knockout',
+        './components',
+        'knockout-configurator',
+        'router',
+        'dialoger',
+        'modaler'
     ],
-    function(framework, components) {
-    	'use strict';
-        
-    	components.registerComponents();
-    	
-        framework.init();
+    function(ko, components, knockoutConfigurator, router, dialoger, modaler) {
+        'use strict';
+
+        components.registerComponents();
+
+        knockoutConfigurator.configure();
+
+        ko.applyBindings({
+            router: router,
+            dialoger: dialoger,
+            modaler: modaler
+        });
     });
