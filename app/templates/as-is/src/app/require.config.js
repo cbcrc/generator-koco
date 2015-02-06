0b11fee63afa2328d7f07fad51be6709c8cdb62b
W3Max <info@maximeseguin.com>
2015-02-06 13:21:36 -0500

    -

--- app/templates/as-is/src/app/require.config.js
+++ app/templates/as-is/src/app/require.config.js
@@ -1,13 +1,14 @@
 var require = {
     baseUrl: '.',
     paths: {
+        'configs': './configs/configs.js',
         'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap',
         'jquery': '../bower_components/jquery/dist/jquery',
         'byroads': '../bower_components/byroads.js/dist/byroads'<% if(useHash) { %>,
+        'signals': '../bower_components/js-signals/dist/signals',
         'hasher': '../bower_components/hasher/dist/js/hasher'<% } %>,
         'knockout': '../bower_components/knockout/dist/knockout',
         'knockout-mapping': '../bower_components/knockout-mapping/knockout.mapping',
-        'signals': '../bower_components/js-signals/dist/signals',
         'text': '../bower_components/requirejs-text/text',
         'lodash': '../bower_components/lodash/lodash',
         'knockout-validation': '../bower_components/knockout-validation/dist/knockout.validation',
