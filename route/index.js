37dc758a473616c479bf759267cb560010343fad
MAXIME SEGUIN <MAXIME.FORGET-SEGUIN@RADIO-CANADA.CA>
2015-02-18 17:10:02 -0500

    -

--- route/index.js
+++ route/index.js
@@ -3,7 +3,7 @@ var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var fs = require('fs');
 
-var ComponentGenerator = generators.NamedBase.extend({
+var ComponentGenerator = generators.Base.extend({
 
     // The name `constructor` is important here
     // constructor: function() {
@@ -22,9 +22,6 @@ var ComponentGenerator = generators.NamedBase.extend({
         this.codeFileExtension = '.js';
         this.startupFile = 'src/app/components' + this.codeFileExtension;
 
-        //TODO: Instead of dasherize, throw exception if name invalid?
-        this.route = this.name;
-
         if (!this.fs.exists(this.startupFile)) {
             this.log(chalk.magenta('The ') + chalk.green('components') + chalk.magenta(' file is missing in the ') + chalk.green('src/app/') + chalk.magenta(' directory.'));
             this.log(chalk.magenta('Scaffolding aborted.'));
@@ -33,21 +30,19 @@ var ComponentGenerator = generators.NamedBase.extend({
 
         this.startupFileContent = this.fs.read(this.startupFile);
 
-        var existingRegistrationRegex1 = new RegExp('\\brouter\\.addRoute\\(\s*[\'"]' + this.route + '[\'"]');
-
-        if (existingRegistrationRegex1.exec(this.startupFileContent)) {
-            this.log(chalk.magenta('The route ') + chalk.green(this.filename) + chalk.magenta(' is already added in the ') + chalk.green('components') + chalk.magenta(' file.'));
-            this.log(chalk.magenta('Scaffolding aborted.'));
-            process.exit(1);
-        }
+        
     },
 
     prompting: function() {
         var done = this.async();
         this.prompt([{
+            name: 'pattern',
+            message: 'What is the pattern of the route?',
+            default: ''
+        }, {
             name: 'pageName',
             message: 'To which page is this route attached?',
-            default: this.route
+            required: true
         }, {
             name: 'title',
             message: 'What\'s the default page title for this route?',
@@ -55,11 +50,21 @@ var ComponentGenerator = generators.NamedBase.extend({
         }], function(answers) {
             this.pageName = answers.pageName;
             this.title = answers.title;
+            this.pattern = answers.pattern;
             done();
         }.bind(this));
     },
 
     validating: function() {
+        var existingRegistrationRegex = new RegExp('\\brouter\\.addRoute\\(\s*[\'"]' + this.pattern + '[\'"]');
+
+        if (existingRegistrationRegex.exec(this.startupFileContent)) {
+            this.log(chalk.magenta('The route ') + chalk.green(this.filename) + chalk.magenta(' is already added in the ') + chalk.green('components') + chalk.magenta(' file.'));
+            this.log(chalk.magenta('Scaffolding aborted.'));
+            process.exit(1);
+        }
+
+
         var filename = this._.dasherize(this.pageName);
 
         var existingRegistrationRegex1 = new RegExp('\\brouter\\.registerPage\\(\s*[\'"]' + filename + '[\'"]');
@@ -72,11 +77,11 @@ var ComponentGenerator = generators.NamedBase.extend({
     },
 
     writing: function() {
-        this.log(chalk.white('Adding the route ') + chalk.green(this.name) + chalk.white(' ...'));
+        this.log(chalk.white('Adding the route ') + chalk.green(this.pattern) + chalk.white(' ...'));
 
         var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]';
         var regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm');
-        var lineToAdd = 'router.addRoute(\'' + this.name + '\'{ pageName: '+ this.pageName +', title: ' + this.title +' });';
+        var lineToAdd = 'router.addRoute(\'' + this.pattern + '\', { pageName: \''+ this.pageName +'\', title: \'' + this.title +'\' });';
         var newContents = this.startupFileContent.replace(regex, '$1' + lineToAdd + '\n$&');
 
         //we write with fs (not this.fs) directly so there is no conflicter in play for this file
